4409feb23084766adbf7ecbb21c7a2ed81b20f71
lyndontavares <integraldominio@gmail.com>
2020-02-29 10:56:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9-covalent-start-template
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9-covalent-start-template)
---
+++ app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+import { CovalentCommonModule } from '@covalent/core/common';
+import { CovalentLayoutModule } from '@covalent/core/layout';
+import { CovalentMediaModule } from '@covalent/core/media';
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot([]),
+    HttpClientModule,
+    /** Material Modules */
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    /** Covalent Modules */
+    CovalentCommonModule,
+    CovalentLayoutModule,
+    CovalentMediaModule,
+    ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
